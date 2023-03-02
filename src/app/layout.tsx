6d6df5ef589c990426ef17fad02f29fa77632d5a
saadfrhan <saadfarhan347@gmail.com>
2023-03-02 11:44:53 +0500

    feat: setup analytics pkg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import { AnalyticsWrapper } from '../components/analytics';
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
     </html>
   )
 }
